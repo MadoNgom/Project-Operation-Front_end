f68911aece86f5a2c75cfc82253fcfc2c74fb346
Mado Ngome <maladongom104@gmail.com>
2025-07-24 14:06:10 +0000

    feat: Setup Authentification services

---
+++ src/app/authentification/services/jwt/jwt.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Jwt {
+  // Set token
+  set token(id_token: string) {
+    localStorage.setItem('auth_token', id_token);
+  }
+  // Get token
+  get token(): string | null {
+    return localStorage.getItem('auth_token');
+  }
+  // Check if the token is valid
+  isTokenValid(): boolean {
+    const token = this.token;
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const decodedToken: any = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      return decodedToken.exp > currentTime;
+    } catch (error) {
+      console.error('Invalid token:', error);
+      return false;
+    }
+  }
+}
