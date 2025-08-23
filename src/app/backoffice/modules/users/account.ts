b9d43ef50fafb1f1a57c374a8c57fcc6fa12fdf8
Mado Ngome <maladongom104@gmail.com>
2025-08-23 14:12:40 +0000

    feat:add account dashboard

--- src/app/backoffice/modules/users/account.ts
+++ src/app/backoffice/modules/users/account.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChartData, ChartOptions, ChartConfiguration } from 'chart.js';
+import { Sidebar } from '../../shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-account',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
-export class Account {}
+export class Account {
+  lineChartData: ChartData<'line'> = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    datasets: [
+      {
+        data: [5000, 12000, 8000, 15000, 14000, 20000, 17000],
+        label: 'Balance',
+        borderColor: 'rgb(34,197,94)',
+        backgroundColor: 'rgba(34,197,94,0.2)',
+        fill: true,
+        tension: 0.4,
+      },
+    ],
+  };
+
+  lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+}
--- src/app/backoffice/shared/sidebar/sidebar.ts
+++ src/app/backoffice/shared/sidebar/sidebar.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
+  standalone: true,
   imports: [],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css'
+  styleUrl: './sidebar.css',
 })
-export class Sidebar {
-
-}
+export class Sidebar {}
